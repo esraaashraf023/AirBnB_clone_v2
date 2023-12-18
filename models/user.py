817cbb295714d1324851e4628290d73be6a8a749
esraaashraf023 <esraaashraf780567@gmail.com>
2023-12-18 14:16:12 -0800

    updata file7

--- models/user.py
+++ models/user.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-This module defines a User class.
-"""
+"""This module defines a class User"""
 
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
 
 class User(BaseModel, Base):
-    """A class for a User object"""
-    __tablename__ = 'users'
+    """This class defines a user by various attributes"""
+
     email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    password =  Column(String(128), nullable=False)
+    first_name =  Column(String(128), nullable=True)
+    last_name =  Column(String(128), nullable=True)
