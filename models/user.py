3eee545f0c2d37684a2408ece8b647de9064b285
esraaashraf023 <esraaashraf780567@gmail.com>
2023-12-18 16:06:21 -0800

    UPdata

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base import Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
