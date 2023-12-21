642b59ab10dee74d8ad204b2cbc4b19b7dc81415
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-12-21 02:04:22 +0200

    task 7

--- models/place.py
+++ models/place.py
@@ -32,8 +32,8 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship('Review', backref='place',  cascade='delete')
     amenity_ids = []
+    reviews = relationship('Review', backref='place',  cascade='delete')
     amenities = relationship('Amenity', secondary='place_amenity',
                              overlaps="place_amenities", viewonly=False)
 
