6911818ea618dc4e34ed059638c99416445f799c
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-01-12 05:14:25 +0200

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
+sudo apt-get update
+sudo apt-get -y install nginx
+
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared/
 
-echo "<html>
+sudo sh -c 'echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>" >/data/web_static/releases/test/index.html'
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 
-sudo chown -R 1000:1000 /data/
-
-sudo sed -i "/server_name _;/a location /hbnb_static {\n\talias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default
+# add config file
+sudo sh -c 'echo "
+server {
+    listen 80;
+    add_header X_Served_by \$HOSTNAME;
+    listen [::]:80 default_server;
+    server_name tuttrue.tech;
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}" > /etc/nginx/sites-available/default'
 
 sudo service nginx restart
-exit 0
