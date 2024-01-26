112351b7938b63fa7c2c3725cf95c0fba04ab957
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-01-26 16:42:29 +0200

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""script that starts a Flask web application"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
 def route():
+    """route function to return Hello HBNB!"""
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
+    """main function to run"""
     app.run(debug=True, host='0.0.0.0', port=5000)
